a8c1350da66e458dbd6b778927753d9f4904a33b
EDUARDO REZENDE <125167885+EduardoRez3nde@users.noreply.github.com>
2023-05-04 19:36:35 -0400

    mundo2 finalizado

--- desafios/ex071.py
+++ desafios/ex071.py
@@ -5,9 +5,9 @@ total = mais = cont = item = menor = 0
 while True:
     produto = str(input('Nome do Produto: '))
     preco = float(input('Preço: R$'))
-    resp = ' '
     total += preco
     cont += 1
+    resp = ' '
     while resp not in 'SN':
         resp = str(input('Quer Continuar? [S/N]')).upper().strip()[0]
     if preco >= 1000:
@@ -21,7 +21,7 @@ while True:
             menor = preco
     if resp == 'N':
         break
-print('======== FIM DO PROGRMA ========')
+print('======== FIM DO PROGRAMA ========')
 print(f'Total da Compra: R${total:.2f}')
 print(f'Temos {mais} produtos custando mais de R$1000')
-print(f'O produto mais barato foi {produto} que custa R${menor:.2f}')
+print(f'O produto mais barato foi {item} que custa R${menor:.2f}')
--- desafios/ex072.py
+++ desafios/ex072.py
@@ -1,2 +1,27 @@
-
+print('='*40)
+print('         BANCO CURSO PYTHON')
+print('='*40)
 valor = int(input('Qual valor quer sacar? '))
+total = valor
+cedula = 50
+tot_cedula = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        tot_cedula += 1
+    else:
+        if tot_cedula > 0:
+            print(f'Total de {tot_cedula} cédulas de R${cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 5
+        elif cedula == 5:
+            cedula = 1
+        tot_cedula = 0
+        if total == 0:
+            break
+print('='*40)
+print('Volte sempre, ao banco CURSO PYTHON.')
